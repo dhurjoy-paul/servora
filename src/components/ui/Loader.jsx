6fed9ab8da813b21743db6f58513e6c31d1a8706
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-07-06 22:11:57 +0600

    Added 404 page

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -4,7 +4,7 @@ import { HashLoader } from 'react-spinners'
 
 const Loader = () => {
   return (
-    <div className="bg-[#D3E1FA] dark:bg-[#07142F] flex items-center justify-center h-screen w-full">
+    <div className="bg-menu/0 dark:bg-[#07142F] flex items-center justify-center h-screen w-full">
       <div className="flex flex-col items-center">
         <HashLoader color="#36D7B7" size={100} speedMultiplier={1.2} />
         <p className="mt-6 text-lg font-semibold">Loading, please wait...</p>
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,25 +1,48 @@
-import { isRouteErrorResponse, useRouteError } from "react-router";
+import Lottie from "lottie-react";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router";
+import notFoundAnimation from "../assets/lottieflow-404-servora.json";
+import Navbar from '../components/Navbar';
+import GoTopBtn from "../components/ui/GoTopBtn";
+import ThemeToggle from "../components/ui/ThemeToggle";
+import ThemeProvider from "../contexts/ThemeContext";
 
 const NotFoundPage = () => {
   const error = useRouteError();
 
-  let title = "Unexpected Error";
-  let message = "Something went wrong.";
-
-  // If it's a standard route error (like 404, 500), customize message
   if (isRouteErrorResponse(error)) {
-    title = `Error ${error.status}`;
-    message = error.statusText || "An error occurred.";
-    console.log(error);
+    console.error(`Route Error: ${error.status} - ${error.statusText}`);
   } else if (error instanceof Error) {
-    message = error.message;
+    console.error(`Unhandled Error: ${error.message}`);
+  } else {
+    console.error("Unknown error:", error);
   }
 
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>{title}</h1>
-      <p>{message}</p>
-    </div>
+    <ThemeProvider>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-menu/0 dark:bg-[#07142F] text-text text-center p-4">
+        <Navbar />
+        <div className="w-full max-w-md">
+          <Lottie animationData={notFoundAnimation} loop={true} />
+        </div>
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white mt-4">
+          Oops! Page Not Found
+        </h1>
+        <p className="text-gray-600 dark:text-gray-300 mt-2 mb-6">
+          The page you’re looking for doesn’t exist or has been moved.
+        </p>
+        <Link
+          to="/"
+          className="inline-block px-6 py-2 text-white bg-brand hover:bg-brand rounded-lg shadow-md transition"
+        >
+          Back to Home
+        </Link>
+        <section className="fixed bottom-4 right-0 -translate-x-1/2 z-500 flex flex-col items-center space-y-2">
+          <GoTopBtn />
+          <ThemeToggle />
+        </section>
+      </div>
+    </ThemeProvider >
   );
-}
-export default NotFoundPage
+};
+
+export default NotFoundPage;
